0be7e6428a9f9f3fc8bdbcea052bb8ccf73e348a
ilhamdika <93665288+ilhamdika@users.noreply.github.com>
2023-06-19 21:05:42 +0700

    feat: slicing about me

--- resources/js/Pages/Prototype/AboutMe.jsx
+++ resources/js/Pages/Prototype/AboutMe.jsx
@@ -1,9 +1,17 @@
 import LayoutPage from "@/Layouts/LayoutPage";
-import { Link } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
+import Flickity from "react-flickity-component";
+
 
 export default function AboutMe() {
+  
+
     return<LayoutPage>
-        <div className="grid grid-cols-2 laptop:flex-row dekstop:flex-row tablet:flex-row mobile:h-auto mobile:grid-cols-1 py-32">
+        <Head>
+            <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css" />
+            <title>Dashboard</title>
+        </Head>
+        <div className="grid grid-cols-2 dekstop:h-screen laptop:h-screen laptop:flex-row dekstop:flex-row tablet:flex-row mobile:h-auto mobile:grid-cols-1 pt-32">
             <div className="">
             <div className="flex justify-center">
                 <div className="">
@@ -35,7 +43,7 @@ export default function AboutMe() {
                 </div>
             </div>
             </div>
-            <div className="p-4">
+            <div className="p-4 mt-4">
                 <h1 className="text-white text-3xl font-thin">About me</h1>
                 <p className="text-white text-xl font-thin mt-3">
                 I am a fresh graduate of S1 Informatics Engineering who is always motivated and able to work together in a team or work individually. Now looking for a position as a web developer. Someone who is thorough, structured, and nimble when doing tasks. Ambitious to learn and grow from the experiences I've had.
@@ -44,9 +52,40 @@ export default function AboutMe() {
         </div>
 
         <div>
-            <h1 className="text-white text-3xl font-thin text-center">My Skills</h1>
-            <div className="flex flex-row">
-                <img src="/images/html.png" className="w-20 h-20 mx-3 my-2"/>
+            <h1 className="text-white text-3xl font-bold text-center">My Skills</h1>
+            
+                <div className="dekstop:grid-cols-9 laptop:grid-cols-9 tablet:grid-cols-7 flex flex-wrap justify-center">
+                
+                    {[1,2,3,4,5,6,7,8,9].map(i=>(
+                        <img key={i}
+                        src="/images/laravel.png"
+                        className="w-28 h-28 max-w-sm mobile:w-14 mobile:h-14 tablet:w-24 mx-3 my-2"
+                        />
+                    ))}
+                
+                </div>
+            
+        </div>
+
+        {/* Edication */}
+
+        <div className="mt-3">
+            <h1 className="text-white text-3xl font-bold text-center">Education</h1>
+            <div className="flex justify-center items-center py-2">
+                <div className="">
+                    <img src="/images/graduate.png" alt="IMG-20211017-123751" border="0" className='w-32 h-32 mx-auto'/>
+                    <h1 className="text-white justify-center text-center items-center flex text-3xl font-thin mt-2">
+                        2018 - 2022
+                    </h1>
+                    <h1 className="text-white justify-center text-center items-center flex text-3xl font-thin mt-2">
+                        Bachelor of Computer Science
+                    </h1>
+                    <h1 className="text-white justify-center text-center items-center flex text-3xl font-thin mt-2">
+                        Muria Kudus University
+                    </h1>
+                    
+                    
+                </div>
             </div>
         </div>
     </LayoutPage>
