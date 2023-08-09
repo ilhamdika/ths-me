00853b0b5a6a1e99ffcaa085d35ed6c317a5736c
ilhamdika <93665288+ilhamdika@users.noreply.github.com>
2023-08-09 21:41:21 +0700

    feat: migration and model database

---
+++ database/migrations/2023_08_09_143825_create_abouts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('profession');
+            $table->string('description');
+            $table->string('image');
+            $table->string('email');
+            $table->string('instagram');
+            $table->string('github');
+            $table->string('linkedin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
